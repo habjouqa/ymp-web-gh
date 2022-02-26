caf12312c8a8c5947fd42b38d37190ae66e954ec
Habjouqa <m.habjouqa@inspirejo.com>
2022-02-27 01:17:17 +0300

    fiix homepage

---
+++ precache-manifest.31c5f840964ca522b3a477a8224612f9.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bc2d545d8f096751237f3c1da4382c",
+    "url": "/medical-portal-theme/index.html"
+  },
+  {
+    "revision": "180477137e98bf641b85",
+    "url": "/medical-portal-theme/static/css/2.bd4a83ea.chunk.css"
+  },
+  {
+    "revision": "0dfc7782be9455168137",
+    "url": "/medical-portal-theme/static/css/main.669e70cb.chunk.css"
+  },
+  {
+    "revision": "180477137e98bf641b85",
+    "url": "/medical-portal-theme/static/js/2.09937e15.chunk.js"
+  },
+  {
+    "revision": "0dfc7782be9455168137",
+    "url": "/medical-portal-theme/static/js/main.cf449f02.chunk.js"
+  },
+  {
+    "revision": "afd09ef0a9107bdafd23",
+    "url": "/medical-portal-theme/static/js/runtime~main.2cb44092.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/medical-portal-theme/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/medical-portal-theme/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/medical-portal-theme/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/medical-portal-theme/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "b3a5c31829d330dea580d81ee5f39d36",
+    "url": "/medical-portal-theme/static/media/avatar-01.b3a5c318.jpg"
+  },
+  {
+    "revision": "cbe9502f21828bb25ad2c44f8a0665e0",
+    "url": "/medical-portal-theme/static/media/banner-01.cbe9502f.jpg"
+  },
+  {
+    "revision": "dca474b417f3783467571a8794938ba6",
+    "url": "/medical-portal-theme/static/media/banner-02.dca474b4.jpg"
+  },
+  {
+    "revision": "fe0dc526cb508865e42bcb508b61af58",
+    "url": "/medical-portal-theme/static/media/banner-02.fe0dc526.jpg"
+  },
+  {
+    "revision": "375a4a9d6867cf38d32ccdd1d87adea3",
+    "url": "/medical-portal-theme/static/media/banner-2.375a4a9d.jpg"
+  },
+  {
+    "revision": "81b25ac2ffb002b130729fba9b1e8545",
+    "url": "/medical-portal-theme/static/media/banner-3.81b25ac2.jpg"
+  },
+  {
+    "revision": "3aceb360cc1b1d3317b91135c4882688",
+    "url": "/medical-portal-theme/static/media/doctor-bg.3aceb360.jpg"
+  },
+  {
+    "revision": "4775b5118c6ffcceb3c22101ce5ff37d",
+    "url": "/medical-portal-theme/static/media/doctor-right-bg.4775b511.jpg"
+  },
+  {
+    "revision": "bb19fa13382482359618a067050080d5",
+    "url": "/medical-portal-theme/static/media/doctor-thumb-01.bb19fa13.jpg"
+  },
+  {
+    "revision": "00c4a45bc479f436de85b22baf15d5f9",
+    "url": "/medical-portal-theme/static/media/down-arrow-gray-dark.00c4a45b.svg"
+  },
+  {
+    "revision": "55cf183e6bb104abc1b7c82ad8b58206",
+    "url": "/medical-portal-theme/static/media/down-arrow-gray.55cf183e.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/medical-portal-theme/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/medical-portal-theme/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/medical-portal-theme/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/medical-portal-theme/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/medical-portal-theme/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/medical-portal-theme/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "9443c1e7fdf95d739af3f0901224f90a",
+    "url": "/medical-portal-theme/static/media/fa-regular-400.9443c1e7.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/medical-portal-theme/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/medical-portal-theme/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/medical-portal-theme/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/medical-portal-theme/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/medical-portal-theme/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/medical-portal-theme/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/medical-portal-theme/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/medical-portal-theme/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2ba34db7fb1c319e91ba098c4250aab2",
+    "url": "/medical-portal-theme/static/media/feathericon.2ba34db7.eot"
+  },
+  {
+    "revision": "2da0719c5095d32f9e3f284ffed19a15",
+    "url": "/medical-portal-theme/static/media/feathericon.2da0719c.ttf"
+  },
+  {
+    "revision": "446775fb9e5115f55d928227b499a3b5",
+    "url": "/medical-portal-theme/static/media/feathericon.446775fb.svg"
+  },
+  {
+    "revision": "6b36f567c009df0a476d61999d381d2f",
+    "url": "/medical-portal-theme/static/media/feathericon.6b36f567.woff"
+  },
+  {
+    "revision": "38dd3d222f7863431f737f482d931395",
+    "url": "/medical-portal-theme/static/media/feature-06.38dd3d22.jpg"
+  },
+  {
+    "revision": "d6fde4f9d9a385ef7e53e3719ee557e2",
+    "url": "/medical-portal-theme/static/media/feature.d6fde4f9.png"
+  },
+  {
+    "revision": "8a9b4d392c18ec028eac31458a591f9d",
+    "url": "/medical-portal-theme/static/media/featured-bg.8a9b4d39.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/medical-portal-theme/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/medical-portal-theme/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/medical-portal-theme/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/medical-portal-theme/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/medical-portal-theme/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9116742edd1633415396879113180c1e",
+    "url": "/medical-portal-theme/static/media/patient.9116742e.jpg"
+  },
+  {
+    "revision": "536fd49aac3b3f339e96f0dc83ebda27",
+    "url": "/medical-portal-theme/static/media/patient2.536fd49a.jpg"
+  },
+  {
+    "revision": "1726b193a147b5f2f4fcd11d3df52ac8",
+    "url": "/medical-portal-theme/static/media/patient3.1726b193.jpg"
+  },
+  {
+    "revision": "1ce340a4f49302c69bce1e585ed4340b",
+    "url": "/medical-portal-theme/static/media/search-bg.1ce340a4.png"
+  },
+  {
+    "revision": "67633c9e71aefe357155d66b39919314",
+    "url": "/medical-portal-theme/static/media/search-icon.67633c9e.svg"
+  },
+  {
+    "revision": "1534e787c477851b02f2da2d1ec515fd",
+    "url": "/medical-portal-theme/static/media/slider-bg.1534e787.jpg"
+  },
+  {
+    "revision": "2b5a2fdffdd3ddb72b4079dffcbdba30",
+    "url": "/medical-portal-theme/static/media/special-banner.2b5a2fdf.jpg"
+  },
+  {
+    "revision": "7a3fa59d7ee92e365a1dfa0b75d95365",
+    "url": "/medical-portal-theme/static/media/specialist-bg.7a3fa59d.jpg"
+  },
+  {
+    "revision": "ba827f3d1e798e7d121bcccb3a9249fe",
+    "url": "/medical-portal-theme/static/media/tick-icon.ba827f3d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66c7cbc21370da18163c1a00e2a565f0.js"
+  "/medical-portal-theme/precache-manifest.31c5f840964ca522b3a477a8224612f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/medical-portal-theme/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
